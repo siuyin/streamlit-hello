e8d4f6c7ea7f162078e44e571b2d4ac0fc4b236b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-02-17 13:42:21 +0800

    feat: main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Streamlit: Hello World")
+st.title("🎈 Streamlit: Hello World!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
